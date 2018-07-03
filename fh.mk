3a9f6eb58a3d4b142d0752dbe690a82934e5fae4
Curtis G <neporood@gmail.com>
2018-07-03 15:55:46 -0500

    Set fingerprint to June OPM6.171019.030.E1
    Also did a little cleanup
    
    Change-Id: Ie452c7aa1570ac869c148ca8a45b67083c121ea2

--- fh.mk
+++ fh.mk
@@ -7,10 +7,10 @@ $(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
+#$(call inherit-product, vendor/nepo/my-apps.mk)
 
 # Disable default FH apps
 DISABLE_CNM := true
-DISABLE_CWB := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := fh_angler
@@ -21,6 +21,6 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM6.171019.030.B1 4768815 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM6.171019.030.E1 4805388 release-keys"
 
-BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM6.171019.030.B1/4768815:user/release-keys
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM6.171019.030.E1/4805388:user/release-keys
